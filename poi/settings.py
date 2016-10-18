712d47efa3d4629b91e9e7923069b07245e21257
fabianvs <fabian@MBP-de-Fabian.home>
2016-10-18 04:56:02 -0300

    Version 0.1
    Configurando MYSQL

--- poi/settings.py
+++ poi/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'poi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'poi',
+        'USER': 'oxido',
+        'PASSWORD': 'oxipassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
